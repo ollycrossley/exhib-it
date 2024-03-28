ad82c79eb1c46a191843b12a2808f23eeeff40dc
Oliver Crossley <ollythecrossley@outlook.com>
2024-03-28 19:41:11 +0000

    adds library page

---
+++ app/library/page.js
@@ -0,0 +1,13 @@
+import NavBar from "@/app/components/NavBar";
+
+export default function Library() {
+
+    return (
+        <main>
+            <NavBar/>
+            <br/>
+            <h1 className={"title has-text-centered"}>Library</h1>
+
+        </main>
+    )
+}
