dff9a0d18792c60681bc27f5abf555d47f78e06c
Oliver Crossley <ollythecrossley@outlook.com>
2024-03-28 19:44:34 +0000

    adds shadow to nav bar for separation

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -4,31 +4,33 @@ import React, {useState} from 'react';
 const NavBar = () => {
     const [isActive, setIsActive] = useState(false)
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <p className={"navbar-item"}><strong>Exhib-It!</strong></p>
-                <a role="button" className={`navbar-burger ${isActive ? "is-active" : null}`} aria-label="menu"
-                   aria-expanded="false"
-                   data-target="main-nav" onClick={() => setIsActive(!isActive)}>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+        <nav className="navbar has-shadow" role="navigation" aria-label="main navigation">
+            <div className={"container"}>
+                <div className="navbar-brand">
+                    <p className={"navbar-item"}><strong>Exhib-It!</strong></p>
+                    <a role="button" className={`navbar-burger ${isActive ? "is-active" : null}`} aria-label="menu"
+                       aria-expanded="false"
+                       data-target="main-nav" onClick={() => setIsActive(!isActive)}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
 
-            <div id="main-nav" className={`navbar-menu ${isActive ? "is-active" : null}`}>
+                <div id="main-nav" className={`navbar-menu ${isActive ? "is-active" : null}`}>
 
-                <div className="navbar-end">
-                    <a className={"navbar-item"} href={"#home-hero"}>
-                        <strong>Home</strong>
-                    </a>
-                    <a className={"navbar-item"} href={"#about-hero"}>
-                        <strong>Library</strong>
-                    </a>
-                    <a className={"navbar-item"} href={"#projects-hero"}>
-                        <strong>My Exhibit</strong>
-                    </a>
+                    <div className="navbar-end">
+                        <a className={"navbar-item"} href={"/"}>
+                            <strong>Home</strong>
+                        </a>
+                        <a className={"navbar-item"} href={"/library"}>
+                            <strong>Library</strong>
+                        </a>
+                        <a className={"navbar-item"} href={"/my-exhibit"}>
+                            <strong>My Exhibit</strong>
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
