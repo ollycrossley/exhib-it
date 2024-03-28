08650c97918b15aa0624513408c71eb5ffa4a50a
Oliver Crossley <ollythecrossley@outlook.com>
2024-03-28 19:41:35 +0000

    add exhibition page

---
+++ app/my-exhibit/page.js
@@ -0,0 +1,12 @@
+import NavBar from "@/app/components/NavBar";
+
+export default function MyExhibit() {
+
+    return (
+        <>
+            <NavBar/>
+            <br/>
+            <h1 className={"title has-text-centered"}>My Exhibit</h1>
+        </>
+    )
+}
