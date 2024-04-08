e97c50d1a9e1c2d1bcd6631f2c1eb017471e8c18
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-08 18:13:36 +0100

    adds image overlap with details

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -1,7 +1,15 @@
 export default function LibraryElement({element}) {
     return (
         <div className={"column is-narrow"}>
-            <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
+            <div className={"img-container"}>
+                <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
+                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-center p-2 is-flex-direction-row"}>
+                    <div>
+                        <p className={"is-white is-size-5 has-text-centered is-bold"}>{element.primaryTitle || "Unknown"}</p>
+                        <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
