7f54be0f77903722671e753fa3b8b81898345e09
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-08 15:57:17 +0100

    api function formats outputs on the fly

--- api.js
+++ api.js
@@ -7,7 +7,17 @@ const baseUrl = "https://api.vam.ac.uk/v2/objects"
 export const getObjectsBySearchVA = async (q) => {
     try {
         const response = await axios.get(`${baseUrl}/search?q=${q}`)
-        return response.data.records
+        const vaObjects = response.data.records
+        const vaImageObjs = vaObjects.filter(obj => obj["_images"]["_iiif_image_base_url"] !== undefined)
+        return vaImageObjs.map(obj => {
+            return {
+                id: obj["systemNumber"],
+                primaryTitle: obj["_primaryTitle"],
+                primaryMaker: obj["_primaryMaker"]["name"],
+                image: `${obj["_images"]["_iiif_image_base_url"]}full/full/0/default.jpg`
+            }
+        })
+
     } catch (error) {
         console.log(error);
     }
