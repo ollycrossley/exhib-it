c626941bf6dec83e9bb62cc65ad339e0bd47dc07
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-12 12:52:08 +0100

    updates library elements with an add to exhibit button

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -1,12 +1,26 @@
+import { useAppContext } from '../context/state';
+
 export default function LibraryElement({element}) {
+
+    const appContext = useAppContext();
+
+    const handleExhibitAdd = () => {
+        appContext.userData.push(element)
+        alert("Item added!")
+        console.log(appContext.userData)
+    }
+
     return (
         <div className={"column is-narrow"}>
             <div className={"img-container"}>
                 <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
-                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-center p-2 is-flex-direction-row"}>
-                    <div>
+                <div
+                    className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row"}>
+                    <div className={"is-flex is-flex-direction-column is-align-items-center"}>
                         <p className={"is-white is-size-5 has-text-centered is-bold"}>{element.primaryTitle || "Unknown"}</p>
                         <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
+                        <br/>
+                        <button className={"button"} onClick={handleExhibitAdd}>Add</button>
                     </div>
                 </div>
             </div>
