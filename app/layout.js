471084869c41eb728d2b20de95d7deed8b8073eb
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-12 12:52:51 +0100

    adds context provider to layout

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,25 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.scss";
+import {ContextProvider} from "@/app/context/state";
+import {Providers} from "@/app/components/Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
-  title: "Exhib-It! ",
-  description: "Your personal art and museum exhibition builder",
+    title: "Exhib-It! ",
+    description: "Your personal art and museum exhibition builder",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" data-theme={"light"}>
-      <head>
-        <link rel={"icon"} href={"./favicon"} sizes="any"/>
-        <script src="https://kit.fontawesome.com/a1a2d1f110.js" crossOrigin="anonymous" async/>
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <html lang="en" data-theme={"light"}>
+            <head>
+                <link rel={"icon"} href={"./favicon"} sizes="any"/>
+                <script src="https://kit.fontawesome.com/a1a2d1f110.js" crossOrigin="anonymous" async/>
+            </head>
+            <Providers>
+                <body className={inter.className}>{children}</body>
+            </Providers>
+        </html>
+    );
 }
