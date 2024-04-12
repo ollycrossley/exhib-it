1cecdd3568896a2bf9e1165170b7b77b2f26fd61
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-12 12:54:21 +0100

    adds placeholder area to view exhibit items

--- app/my-exhibit/page.js
+++ app/my-exhibit/page.js
@@ -1,12 +1,23 @@
+"use client"
 import NavBar from "@/app/components/NavBar";
+import { useAppContext } from '../context/state';
+import LibraryElement from "@/app/components/LibraryElement";
 
 export default function MyExhibit() {
+
+    const appContext = useAppContext();
 
     return (
         <>
             <NavBar/>
             <br/>
             <h1 className={"title has-text-centered"}>My Exhibit</h1>
+
+            <div className={"container"}>
+                <div className={"columns is-gapless is-gap-1 is-multiline pt-6 is-mobile"}>
+                    {appContext.userData.map(item => <LibraryElement element={item}/>)}
+                </div>
+            </div>
         </>
     )
 }
