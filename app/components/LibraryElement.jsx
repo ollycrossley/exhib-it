39ac43a893c838603e5016c3df72d2b139ad348d
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-12 19:18:24 +0100

    uniforms the grid a little better to look neater

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -12,26 +12,23 @@ export default function LibraryElement({element}) {
     }, [isAdded]);
 
     const handleExhibitAdd = () => {
+        setIsAdded(true)
         appContext.exhibitObjects.push(element)
         appContext.exhibitObjectIds.push(element.intId)
-        setIsAdded(true)
-        alert("Item added!")
-        console.log(appContext.exhibitObjects)
     }
 
     return (
-        <div className={"column is-narrow"}>
-            <div className={"img-container"}>
+        <div className={"column is-flex is-flex-grow-5 is-flex-direction-column is-narrow"}>
+            <div className={"img-container is-flex is-flex-direction-column"}>
                 <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
-                <div
-                    className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row"}>
+                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row"}>
                     <div className={"is-flex is-flex-direction-column is-align-items-center"}>
                         <p className={"is-white is-size-5 has-text-centered is-bold"}>{element.primaryTitle || "Unknown"}</p>
                         <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
                         <br/>
-                        {appContext.exhibitObjectIds.includes(element.intId) ? <button className={"button"} onClick={handleExhibitAdd} disabled={true}>Already
-                            Added</button>
-                            : <button className={"button"} onClick={handleExhibitAdd}>Add</button>}
+                        {appContext.exhibitObjectIds.includes(element.intId)? <button className={"button is-success is-wide"} disabled={true} ><span className="icon is-small" ><i className="fas fa-check"></i></span></button>
+                            : <button className={"button"} onClick={handleExhibitAdd}>Add
+                    </button>}
                     </div>
                 </div>
             </div>
