a99a20cc0758a59d20811749450facd072b7e1ee
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-12 21:45:23 +0100

    added overflow detection functionality

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -3,6 +3,12 @@ import {useEffect, useState} from "react";
 
 export default function LibraryElement({element}) {
 
+    function truncateWithEllipsis(inputString, maxLength) {
+        return inputString.length > maxLength
+            ? inputString.substring(0, maxLength - 3) + "..."
+            : inputString;
+    }
+
     const appContext = useAppContext();
 
     const [isAdded, setIsAdded] = useState(false)
@@ -22,9 +28,9 @@ export default function LibraryElement({element}) {
 
                 <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
 
-                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row"}>
+                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row lib-el-desc"}>
                     <div className={"is-flex is-flex-direction-column is-align-items-center"}>
-                        <p className={"is-white is-size-5 has-text-centered is-bold"}>{element.primaryTitle || "Unknown"}</p>
+                        <p className={"is-white is-size-5 has-text-centered is-bold"}>{ truncateWithEllipsis(element.primaryTitle, 50) || "Unknown"}</p>
                         <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
                         <br/>
                         {appContext.exhibitObjectIds.includes(element.intId)? <button className={"button is-success is-wide"} disabled={true} ><span className="icon is-small" ><i className="fas fa-check"></i></span></button>
