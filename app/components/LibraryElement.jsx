0f01d77773a28414440d7f4974b56d14820ffab0
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-14 20:21:10 +0100

    adds delete button and configures based on being an exhibit item

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
 import {useExhibitObjectContexts} from "@/app/context/exhibitObjects";
 import {useExhibitIdsContext} from "@/app/context/exhibitObjectIds";
+import {prefixes} from "next/dist/build/output/log";
 
-export default function LibraryElement({element}) {
+export default function LibraryElement({element, isMyExhibit = false}) {
 
     function truncateWithEllipsis(inputString, maxLength) {
         return inputString.length > maxLength
@@ -20,27 +21,44 @@ export default function LibraryElement({element}) {
 
     const handleExhibitAdd = () => {
         setIsAdded(true)
-        // appContext.exhibitObjects.push(element)
         setExhibitObjects(prev => [...prev, element])
-        // appContext.exhibitObjectIds.push(element.intId)
         setExhibitIds(prev => [...prev, element.intId])
+    }
+
+    const handleExhibitRemove = () => {
+        setIsAdded(false)
+        const index = exhibitIds.indexOf(element.intId)
+        setExhibitIds(prev => prev.toSpliced(index, 1))
+        setExhibitObjects(prev => prev.toSpliced(index, 1))
+    }
+
+    const handleButtonRack = () => {
+        if (isMyExhibit) {
+            return (
+                <div className={"column is-narrow"}>
+                    <button className={"button is-danger is-wide"} onClick={handleExhibitRemove}><span className="icon is-small"><i
+                        className="fas fa-x"></i></span></button>
+                </div>
+            )
+        } else {
+            return exhibitIds.includes(element.intId) ?
+                <button className={"button is-success is-wide"} disabled={true}><span className="icon is-small"><i className="fas fa-check"></i></span></button>
+                : <button className={"button"} onClick={handleExhibitAdd}>Add</button>
+        }
     }
 
     return (
         <div className={"img-container is-flex is-flex-grow-1 m-1 is-flex-direction-column"}>
-
-                <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
-
-                <div className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row lib-el-desc"}>
-                    <div className={"is-flex is-flex-direction-column is-align-items-center"}>
-                        <p className={"is-white is-size-5 has-text-centered is-bold"}>{ truncateWithEllipsis(element.primaryTitle, 50) || "Unknown"}</p>
-                        <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
-                        <br/>
-                        {exhibitIds.includes(element.intId)? <button className={"button is-success is-wide"} disabled={true} ><span className="icon is-small" ><i className="fas fa-check"></i></span></button>
-                            : <button className={"button"} onClick={handleExhibitAdd}>Add
-                    </button>}
-                    </div>
+            <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
+            <div
+                className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row lib-el-desc"}>
+                <div className={"is-flex is-flex-direction-column is-align-items-center"}>
+                    <p className={"is-white is-size-5 has-text-centered is-bold"}>{truncateWithEllipsis(element.primaryTitle, 50) || "Unknown"}</p>
+                    <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
+                    <br/>
+                    {handleButtonRack()}
                 </div>
+            </div>
 
         </div>
     )
