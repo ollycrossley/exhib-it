552fd650558742db667989d0ada8dbe02b6aa9d9
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-15 12:25:53 +0100

    adds loading state for loading symbol to appear in

--- app/library/page.js
+++ app/library/page.js
@@ -4,15 +4,17 @@ import LibraryElement from "@/app/components/LibraryElement";
 import {useEffect, useState} from "react";
 import {getObjectBySearchHARV, getObjectsBySearchMET, getObjectsBySearchVA} from "@/api";
 import SearchOptionsBar from "@/app/components/SearchOptionsBar";
+import SvgComponent from "@/app/components/MuseumLoadingSymbol";
 
 export default function Library() {
 
     const [searchTerm, setSearchTerm] = useState("")
     const [items, setItems] = useState([])
     const [currentApi, setCurrentApi] = useState("va")
-
+    const [isLoading, setIsLoading] = useState(false)
 
     const getItems = async () => {
+        setIsLoading(true)
         if (searchTerm === "") {
             setItems([])
         } else {
@@ -24,13 +26,21 @@ export default function Library() {
                 setItems(await getObjectsBySearchMET(searchTerm))
             }
         }
+        setIsLoading(false)
     }
 
     const displayItems = () => {
+        if (items !== undefined && items.length > 0) {
+            return (
+                <div className={"lib-grid"}>
+                    {items.map(item => <LibraryElement element={item}/>)}
+                </div>
+            )
+        }
         if (searchTerm === "") {
             return (
                 <div className={"hero has-text-centered"}>
-                    <p className={"hero-body subtitle is-4"}>Search for an item!</p>
+                <p className={"hero-body subtitle is-4"}>Search for an item!</p>
                 </div>
             )
         } else if (items === undefined || items.length === 0) {
@@ -51,6 +61,28 @@ export default function Library() {
     useEffect(() => {
         getItems()
     }, [currentApi]);
+
+    if (isLoading) {
+        return (
+            <main>
+                <NavBar/>
+                <br/>
+                <h1 className={"title has-text-centered"}>Library</h1>
+                <br/><br/><br/>
+                <div className={"container"}>
+                    <SearchOptionsBar currentApi={currentApi} searchTerm={searchTerm} setCurrentApi={setCurrentApi}
+                                      setSearchTerm={setSearchTerm} getItems={getItems}/>
+
+                    <div className={"hero"}>
+                        <div className={"hero-body is-justify-content-center"}>
+                            <SvgComponent />
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+        )
+    }
 
     return (
         <main>
