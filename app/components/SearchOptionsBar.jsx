6474a2b5ab90acc8b3e6e927773f63b396021023
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-15 12:48:41 +0100

    adds some break points for different mobile devices

--- app/components/SearchOptionsBar.jsx
+++ app/components/SearchOptionsBar.jsx
@@ -37,53 +37,147 @@ export default function SearchOptionsBar({searchTerm, setSearchTerm, currentApi,
     }, [isApiDrActive]);
 
     return (
-        <div className={"columns"}>
 
-            <div className={"column is-narrow"}>
-                <div className="field has-addons" id={"search-bar"}>
-                    <div className="control">
-                        <input className="input" type="text" placeholder="Search for item"
-                               onChange={e => setSearchTerm(e.target.value)}
-                               onKeyDown={e => handleSearchKeyPress(e)}/>
-                    </div>
-                    <div className="control">
-                        <button className="button is-info" onClick={getItems}>
-                            Search
-                        </button>
+        <div>
+            <div className={"columns is-hidden-tablet-only is-hidden-mobile"}>
+
+                <div className={"column is-narrow"}>
+                    <div className="field has-addons" id={"search-bar"}>
+                        <div className="control">
+                            <input className="input" type="text" placeholder="Search for item"
+                                   onChange={e => setSearchTerm(e.target.value)}
+                                   onKeyDown={e => handleSearchKeyPress(e)}/>
+                        </div>
+                        <div className="control">
+                            <button className="button is-info" onClick={getItems}>
+                                Search
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={"column is-narrow"}>
+                <div className={"column is-narrow"}>
 
-                <div className={`dropdown ${isApiDrActive ? "is-active" : ""}`} ref={ref}>
-                    <div className="dropdown-trigger">
-                        <button className="button" aria-haspopup="true" aria-controls="dropdown-menu3"
-                                onClick={() => setIsApiDrActive(!isApiDrActive)}>
-                            <p><strong>Museum
-                                Selection</strong> ({Object.keys(acceptedApis).find(api => acceptedApis[api] === currentApi)})
-                            </p>
-                            <span className="icon is-small has-text-info">
+                    <div className={`dropdown ${isApiDrActive ? "is-active" : ""}`} ref={ref}>
+                        <div className="dropdown-trigger">
+                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu3"
+                                    onClick={() => setIsApiDrActive(!isApiDrActive)}>
+                                <p><strong>Museum
+                                    Selection</strong> ({Object.keys(acceptedApis).find(api => acceptedApis[api] === currentApi)})
+                                </p>
+                                <span className="icon is-small has-text-info">
                                 <i className="fas fa-angle-down" aria-hidden="true"></i>
                             </span>
-                        </button>
-                    </div>
+                            </button>
+                        </div>
 
-                    <div className="dropdown-menu" id="dropdown-menu3" role="menu">
-                        <div className="dropdown-content">
-                            {Object.keys(acceptedApis).map(api => <a
-                                className={`dropdown-item ${acceptedApis[api] === currentApi ? "has-background-info-light" : ""}`}
-                                key={api}
-                                onMouseDown={() => {
-                                    handleApi(api)
-                                    setIsApiDrActive(false)
-                                }}>{api}</a>)}
+                        <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                            <div className="dropdown-content">
+                                {Object.keys(acceptedApis).map(api => <a
+                                    className={`dropdown-item ${acceptedApis[api] === currentApi ? "has-background-info-light" : ""}`}
+                                    key={api}
+                                    onMouseDown={() => {
+                                        handleApi(api)
+                                        setIsApiDrActive(false)
+                                    }}>{api}</a>)}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
-            
+
+            <div className={"columns is-centered is-hidden-mobile is-hidden-desktop"}>
+
+                <div className={"column is-narrow"}>
+                    <div className="field has-addons" id={"search-bar"}>
+                        <div className="control">
+                            <input className="input" type="text" placeholder="Search for item"
+                                   onChange={e => setSearchTerm(e.target.value)}
+                                   onKeyDown={e => handleSearchKeyPress(e)}/>
+                        </div>
+                        <div className="control">
+                            <button className="button is-info" onClick={getItems}>
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"column is-narrow"}>
+
+                    <div className={`dropdown ${isApiDrActive ? "is-active" : ""}`} ref={ref}>
+                        <div className="dropdown-trigger">
+                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu3"
+                                    onClick={() => setIsApiDrActive(!isApiDrActive)}>
+                                <p><strong>Museum
+                                    Selection</strong> ({Object.keys(acceptedApis).find(api => acceptedApis[api] === currentApi)})
+                                </p>
+                                <span className="icon is-small has-text-info">
+                                <i className="fas fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                            </button>
+                        </div>
+
+                        <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                            <div className="dropdown-content">
+                                {Object.keys(acceptedApis).map(api => <a
+                                    className={`dropdown-item ${acceptedApis[api] === currentApi ? "has-background-info-light" : ""}`}
+                                    key={api}
+                                    onMouseDown={() => {
+                                        handleApi(api)
+                                        setIsApiDrActive(false)
+                                    }}>{api}</a>)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={"columns is-hidden-desktop is-hidden-tablet is-centered"}>
+
+                <div className={"column"}>
+                    <div className="field has-addons is-justify-content-center" id={"search-bar"}>
+                        <div className="control">
+                            <input className="input" type="text" placeholder="Search for item"
+                                   onChange={e => setSearchTerm(e.target.value)}
+                                   onKeyDown={e => handleSearchKeyPress(e)}/>
+                        </div>
+                        <div className="control">
+                            <button className="button is-info" onClick={getItems}>
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"column has-text-centered"}>
+                    <div className={`dropdown ${isApiDrActive ? "is-active" : ""}`} ref={ref}>
+                        <div className="dropdown-trigger">
+                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu3"
+                                    onClick={() => setIsApiDrActive(!isApiDrActive)}>
+                                <p><strong>Museum
+                                    Selection</strong> ({Object.keys(acceptedApis).find(api => acceptedApis[api] === currentApi)})
+                                </p>
+                                <span className="icon is-small has-text-info">
+                                <i className="fas fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                            </button>
+                        </div>
+
+                        <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                            <div className="dropdown-content">
+                                {Object.keys(acceptedApis).map(api => <a
+                                    className={`dropdown-item has-text-left ${acceptedApis[api] === currentApi ? "has-background-info-light" : ""}`}
+                                    key={api}
+                                    onMouseDown={() => {
+                                        handleApi(api)
+                                        setIsApiDrActive(false)
+                                    }}>{api}</a>)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
