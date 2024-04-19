b8714626b18dcead99015b1b11abb89ff0647ffa
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-19 21:51:10 +0100

    creates slider component

---
+++ app/components/Carousel.jsx
@@ -0,0 +1,30 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from 'react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+export const Carousel = ({photos}) => {
+    return (
+        <>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper block">
+
+                {photos.map((photo, index) => {
+                    console.log(photo)
+                    return (
+                        <SwiperSlide>
+                            <div className={"image is-4by3"}>
+                                <img src={photo} alt="" style={{ objectFit: 'contain' }} />
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
+
+            </Swiper>
+        </>
+    );
+}
