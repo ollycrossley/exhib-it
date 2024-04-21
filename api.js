1816f30115328fbdd8601fcab4a0ec57f340b2fd
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-21 22:36:30 +0100

    update API with advanced data retrieval for single items

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 // VA: https://developers.vam.ac.uk/guide/v2/search/introduction.html
+
 const baseUrl = "https://api.vam.ac.uk/v2/objects"
 
 
@@ -11,10 +12,28 @@ export const getObjectByIdVA = async (id) => {
         data.images = data.images.map(i => {
             return `https://framemark.vam.ac.uk/collections/${i}/full/full/0/default.jpg`
         })
+
+        const categories = data.categories.map(c => { return c.text})
+        const names = data.artistMakerPerson.map(n => { return n.name.text})
+        names.push(...data.artistMakerOrganisations.map(n => { return n.name.text}))
+        console.log(names)
+        let namesString = ""
+        if (names.length > 0) {
+            names.forEach((n, i) => {
+                if (i === 0) {
+                    namesString += `${n}`
+                } else {
+                    namesString += `; ${n}`
+                }
+            })
+        }
         return {
             images: data.images,
-            date: data.productionDates[0].date.text || "Unknown",
-            description: data.briefDescription
+            date: data["productionDates"][0].date.text || "Unknown",
+            description: data["briefDescription"],
+            link: undefined,
+            categories: categories || undefined,
+            credits: namesString || undefined,
         };
     } catch (e) {
         console.error(e)
@@ -33,7 +52,7 @@ export const getObjectsBySearchVA = async (q, page = 1, size = 100) => {
                 primaryTitle: obj["_primaryTitle"],
                 primaryMaker: obj["_primaryMaker"]["name"],
                 image: `${obj["_images"]["_iiif_image_base_url"]}full/full/0/default.jpg`,
-                intId: `va${obj.systemNumber}`
+                intId: `va${obj["systemNumber"]}`
             }
         })
         return uniqify(vaImageObjs, "image")
@@ -52,14 +71,41 @@ export const getObjectByIdHARV = async (id) => {
                 apikey: process.env.NEXT_PUBLIC_HARVARD_API_KEY,
             }
         })
+        const data = response.data
+        data.images = data.images.map(i => {
+            return `${i["iiifbaseuri"]}/full/full/0/default.jpg`
+        })
 
-        return response.data
+
+        const names = data.people.map(i => {
+            return i.alphasort
+        })
+
+        let namesString = ""
+        if (names.length > 0) {
+            names.forEach((n, i) => {
+                if (i === 0) {
+                    namesString += `${n}`
+                } else {
+                    namesString += `; ${n}`
+                }
+            })
+        }
+
+        return {
+            images: [...data.images] || null,
+            date: data["dated"] || null,
+            description: data.description || null,
+            link: data.url || undefined,
+            categories: [data.classification || null, data["culture"] || null, data["technique"] || null].filter(e => e !== null),
+            credits: namesString || null
+        }
     } catch (e) {
         console.error(e)
     }
 }
 
-export const getObjectBySearchHARV = async (q, page = 1, size = 100) => {
+export const getObjectsBySearchHARV = async (q, page = 1, size = 100) => {
     try {
         const response = await axios.get(`https://api.harvardartmuseums.org/object?size=${size}&page=${page}`, {
             params: {
@@ -95,13 +141,14 @@ export const getObjectBySearchHARV = async (q, page = 1, size = 100) => {
 
 
 // MET: https://metmuseum.github.io/#search
+
 const baseUrlMet = "https://collectionapi.metmuseum.org/public/collection/v1/"
 
 
 export const getObjectIdsBySearchMET = async (q) => {
     try {
         const response = await axios.get(`${baseUrlMet}/search?isOnView=true&hasImages=true&q=${q}`)
-        return (response.data.objectIDs).sort((a, b) => a - b)
+        return (response.data["objectIDs"]).sort((a, b) => a - b)
     } catch (error) {
         console.log(error);
     }
@@ -110,7 +157,17 @@ export const getObjectIdsBySearchMET = async (q) => {
 export const getObjectByIdMET = async (id) => {
     try {
         const response = await axios.get(`${baseUrlMet}/objects/${id}`)
-        return response.data
+        const data = response.data
+
+
+        return {
+            images: [data["primaryImage"], ...data["additionalImages"] || null ],
+            date: data["objectDate"],
+            description: data["measurements"]["elementDescription"],
+            link: data["objectURL"] || undefined,
+            categories: [data["culture"] || null, data["medium"] || null, data["country"] || null].filter(e => e !== null),
+            credits: [data["artistAlphaSort"]]
+        }
     } catch (error) {
         console.log(error);
     }
@@ -121,7 +178,7 @@ export const getObjectsBySearchMET = async (q, page = 1, size = 100) => {
         const response = await axios.get(`${baseUrlMet}/search?isOnView=true&hasImages=true&q=${q}`)
         const start = (page * size) - size
 
-        const sortedResponse = (response.data.objectIDs).sort((a, b) => a - b)
+        const sortedResponse = (response.data["objectIDs"]).sort((a, b) => a - b)
 
         const pagedObjectIds = sortedResponse.slice(start, start + (size - 1))
 
@@ -130,15 +187,15 @@ export const getObjectsBySearchMET = async (q, page = 1, size = 100) => {
         const promisedObjects = pagedObjectIds.map(obj => axios.get(`${baseUrlMet}/objects/${obj}`))
         const receivedObjects = await Promise.all(([...promisedObjects]))
         let objectsFormatted = (receivedObjects).map(obj => obj.data)
-        objectsFormatted = objectsFormatted.filter(obj => obj.primaryImage !== '')
+        objectsFormatted = objectsFormatted.filter(obj => obj["primaryImage"] !== '')
         objectsFormatted = objectsFormatted.map(obj => {
             return {
                 apiType: "met",
                 id: obj["objectID"],
                 primaryTitle: obj["title"],
-                primaryMaker: obj["artistAlphasort"],
+                primaryMaker: obj["artistAlphaSort"],
                 image: obj["primaryImage"],
-                intId: `met${obj.objectID}`
+                intId: `met${obj["objectID"]}`
             }
         })
         return uniqify(objectsFormatted, "image")
@@ -147,7 +204,6 @@ export const getObjectsBySearchMET = async (q, page = 1, size = 100) => {
         console.log(error);
     }
 }
-
 
 // GENERIC FUNCTIONS
 
