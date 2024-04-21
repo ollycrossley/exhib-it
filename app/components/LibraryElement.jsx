c40fa9ae6c785874f89245f6aaf9a72fe4330a58
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-21 15:35:06 +0100

    transfer buttons to modal rather than on tab

--- app/components/LibraryElement.jsx
+++ app/components/LibraryElement.jsx
@@ -1,7 +1,3 @@
-"use client"
-import {useEffect, useState} from "react";
-import {useExhibitObjectContexts} from "@/app/context/exhibitObjects";
-import {useExhibitIdsContext} from "@/app/context/exhibitObjectIds";
 import {getObjectByIdHARV, getObjectByIdMET, getObjectByIdVA} from "@/api";
 
 
@@ -12,14 +8,6 @@ export default function LibraryElement({element, isMyExhibit = false, key = elem
             ? inputString.substring(0, maxLength - 3) + "..."
             : inputString;
     }
-
-    const {exhibitObjects, setExhibitObjects} = useExhibitObjectContexts()
-    const {exhibitIds, setExhibitIds} = useExhibitIdsContext()
-    const [isAdded, setIsAdded] = useState(false)
-    const [wholeData, setWholeData] = useState()
-
-    useEffect(() => {
-    }, [isAdded]);
 
     const handleModal = async () => {
         let gotItem = undefined
@@ -38,48 +26,14 @@ export default function LibraryElement({element, isMyExhibit = false, key = elem
         toggleModal()
     }
 
-    const handleExhibitAdd = async () => {
-        setIsAdded(true)
-        setExhibitObjects(prev => [...prev, element])
-        setExhibitIds(prev => [...prev, element.intId])
-    }
-
-    const handleExhibitRemove = () => {
-        setIsAdded(false)
-        const index = exhibitIds.indexOf(element.intId)
-        setExhibitIds(prev => prev.toSpliced(index, 1))
-        setExhibitObjects(prev => prev.toSpliced(index, 1))
-    }
-
-    const handleButtonRack = () => {
-        if (isMyExhibit) {
-            return (
-                <div className={"column is-narrow"}>
-                    <button className={"button is-danger is-wide"} onClick={handleExhibitRemove}><span
-                        className="icon is-small"><i
-                        className="fas fa-x"></i></span></button>
-                    <button className={"button is-danger is-wide"} onClick={handleModal}><span
-                        className="icon is-small"><i
-                        className="fas fa-info"></i></span></button>
-                </div>
-            )
-        } else {
-            return exhibitIds.includes(element.intId) ?
-                <button className={"button is-success is-wide"} disabled={true}><span className="icon is-small"><i className="fas fa-check"></i></span></button>
-                : <button className={"button"} onClick={handleExhibitAdd}>Add</button>
-        }
-    }
-
     return (
-        <div className={"img-container is-flex is-flex-grow-1 m-1 is-flex-direction-column"}>
+        <div className={"img-container is-flex is-flex-grow-1 m-1 is-flex-direction-column is-clickable"} onClick={handleModal}>
             <img className={"image lib-el"} src={element.image} alt={"library element image"}/>
                 <div
                     className={"overlay is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-space-around p-2 is-flex-direction-row lib-el-desc"}>
                     <div className={"is-flex is-flex-direction-column is-align-items-center"}>
                         <p className={"is-white is-size-5 has-text-centered is-bold"}>{truncateWithEllipsis(element.primaryTitle, 50) || "Unknown"}</p>
                         <p className={"is-white is-size-6 has-text-centered"}>{element.primaryMaker || "Unknown"}</p>
-                        <br/>
-                        {handleButtonRack()}
                     </div>
                 </div>
 
--- app/components/LibraryModal.jsx
+++ app/components/LibraryModal.jsx
@@ -1,6 +1,43 @@
 import {Carousel} from "@/app/components/Carousel";
+import {useExhibitObjectContexts} from "@/app/context/exhibitObjects";
+import {useExhibitIdsContext} from "@/app/context/exhibitObjectIds";
+import {useEffect, useState} from "react";
 
-export const LibraryModal = ({item, toggleModal, modalState}) => {
+export const LibraryModal = ({item, toggleModal, modalState, isMyExhibit = false}) => {
+
+    const {exhibitObjects, setExhibitObjects} = useExhibitObjectContexts()
+    const {exhibitIds, setExhibitIds} = useExhibitIdsContext()
+    const [isAdded, setIsAdded] = useState(false)
+
+    const handleExhibitAdd = async () => {
+        setIsAdded(true)
+        setExhibitObjects(prev => [...prev, item])
+        setExhibitIds(prev => [...prev, item.intId])
+    }
+
+    const handleExhibitRemove = () => {
+        setIsAdded(false)
+        const index = exhibitIds.indexOf(item.intId)
+        setExhibitIds(prev => prev.toSpliced(index, 1))
+        setExhibitObjects(prev => prev.toSpliced(index, 1))
+        toggleModal()
+    }
+
+    const handleButtonRack = () => {
+        if (isMyExhibit) {
+            return (
+                    <button className={"button is-danger is-wide"} onClick={handleExhibitRemove}>Remove</button>
+            )
+        } else {
+            return exhibitIds.includes(item.intId) ?
+                <button className={"button is-success is-wide"} disabled={true}><span className="icon is-small"><i className="fas fa-check"></i></span></button>
+                : <button className={"button is-info"} onClick={handleExhibitAdd}>Add to your Exhibit</button>
+        }
+    }
+
+    useEffect(() => {
+
+    }, [isAdded]);
 
     // Don't render if not active
     if(!modalState) {
@@ -20,11 +57,15 @@ export const LibraryModal = ({item, toggleModal, modalState}) => {
                         <div className={"mb-5"}><Carousel photos={[...item.images]}/></div>
                         <p className={"is-text title"}><b> {item["title"] || item["primaryTitle"] || "Untitled"}</b></p>
                         <p className={"is-text"}><b>Date:</b> {item["date"] || "Undated"}</p>
-                        <p className={"is-text"}><b>Description:</b> <i>{item["description"] || "No description given"}</i></p>
+                        <p className={"is-text"}><b>Description:</b>
+                            <i>{item["description"] || "No description given"}</i></p>
+
+                        <hr/>
+                        <p className={"is-text"}><b>See more:</b> { item.link ? <a href={item.link} target="_blank" rel="noopener noreferrer">{item.link}</a> : "No link provided"} </p>
                     </div>
                 </section>
                 <footer className="modal-card-foot">
-                    <a className="button" onClick={toggleModal}>Cancel</a>
+                    {handleButtonRack()}
                 </footer>
             </div>
         </div>
