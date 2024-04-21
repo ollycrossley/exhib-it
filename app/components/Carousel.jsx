55a71ce4bef158317d9da7de099f9f9e9937d52f
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-21 16:25:19 +0100

    adds zoom funtionality instead of linking to avoid misclicks

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {Swiper, SwiperSlide} from 'swiper/react';
 import React from 'react';
 
 // Import Swiper styles
@@ -6,24 +6,26 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 // import required modules
-import { Navigation } from 'swiper/modules';
+import {Navigation, Mousewheel, Zoom} from 'swiper/modules';
 
 export const Carousel = ({photos}) => {
     return (
         <>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper block">
-
+            <Swiper className="mySwiper block"
+                    navigation={true}
+                    modules={[Navigation, Mousewheel, Zoom]}
+                    mousewheel={true}
+                    zoom={true}
+                    grabCursor={true}>
                 {photos.map((photo, index) => {
                     return (
                         <SwiperSlide>
-                            <div className={"image is-4by3"}>
-                                <a href={photo} target={"_blank"} rel="noopener noreferrer"><img src={photo} alt=""
-                                                                                                 style={{objectFit: 'contain'}}/></a>
+                            <div className={"image is-4by3 swiper-zoom-container"}>
+                                <img src={photo} alt="" style={{objectFit: 'contain'}}/>
                             </div>
                         </SwiperSlide>
                     )
                 })}
-
             </Swiper>
         </>
     );
