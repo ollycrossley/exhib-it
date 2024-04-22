50ff867340a23bae2e9ab871f5dae6f65626560a
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-22 03:15:14 +0100

    adds home page

---
+++ app/components/HomeNavBar.jsx
@@ -0,0 +1,41 @@
+"use client"
+import React, {useState} from 'react';
+import Link from "next/link";
+
+const NavBar = () => {
+    const [isActive, setIsActive] = useState(false)
+    return (
+        <nav className="navbar is-fixed-top-touch" role="navigation" aria-label="main navigation">
+            <div className={"container"}>
+                <div className="navbar-brand">
+                    <p className={"navbar-item"}><strong>Exhib-It!</strong></p>
+                    <a role="button" className={`navbar-burger ${isActive ? "is-active" : null}`} aria-label="menu"
+                       aria-expanded="false"
+                       data-target="main-nav" onClick={() => setIsActive(!isActive)}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="main-nav" className={`navbar-menu ${isActive ? "is-active" : null}`}>
+
+                    <div className="navbar-end">
+                        <Link className={"navbar-item"} href={"/"}>
+                            <strong>Home</strong>
+                        </Link>
+                        <Link className={"navbar-item"} href={"/library"}>
+                            <strong>Library</strong>
+                        </Link>
+                        <Link className={"navbar-item"} href={"/my-exhibit"}>
+                            <strong>My Exhibit</strong>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 import NavBar from "@/app/components/NavBar";
+import HomeNavBar from "@/app/components/HomeNavBar";
 
 export default function Home() {
   return (
       <main>
-          <NavBar/>
-          <br/>
-          <h1 className={"title has-text-centered"}>Home</h1>
+
+          <section className={"hero is-fullheight main-hero"}>
+              <HomeNavBar/>
+              <div className={"hero-body mx-auto has-text-centered"}>
+                  <div>
+                      <p className={"title is-1 has-text-centered"}>Exhib-it</p>
+                      <p className={"subtitle is-3 pt-3"}>Your personal gallery</p>
+                  </div>
+              </div>
+          </section>
       </main>
   );
 }
